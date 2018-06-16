d4840d70b683f87b4ad066f9588634e49abdd584
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-06-16 19:59:06 +1000

    make some code easy to read

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 console.log('App.js is running!')
 
-const template = <div><h1>Indecision App</h1> <p>This is some info</p></div>;
+const template = (
+<div>
+    <h1>Indecision App</h1> 
+    <p>This is some info</p>
+    <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+    </ol>
+</div>
+);
 const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
