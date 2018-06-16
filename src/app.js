d3b76798dd64c772fa20e1526c42a5834faa6293
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-06-16 21:18:03 +1000

    rendered strings and numbers

--- src/app.js
+++ src/app.js
@@ -1,15 +1,48 @@
 console.log('App.js is running!')
 
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer'
+}; 
+
 const template = (
 <div>
-    <h1>Indecision App</h1> 
-    <p>This is some info</p>
+    <h1>{app.title}</h1> 
+    <p>{app.subtitle}</p>
     <ol>
         <li>Item one</li>
         <li>Item two</li>
     </ol>
 </div>
 );
+
+// const userName = 'Serina';
+// const userAge = 20;
+// const userLocation = 'Melbourne';
+
+// const templateTwo = (
+//     <div>
+//         <h1>{userName.toUpperCase()}</h1>
+//         <p>Age: {userAge}</p>
+//         <p>Location: {userLocation}</p>
+//     </div>
+// );
+
+const user = {
+    name: 'Serina',
+    age: 20,
+    location: 'Melbourne'
+};
+
+const templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>{user.age}</p>
+        <p>{user.location}</p>
+    </div>
+);
+
 const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
