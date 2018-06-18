10b309a48f0588158beefad59724e50d12839f57
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-06-18 13:59:00 +1000

    add logic to cause the count to be incremented

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ const template = (
  
 let count = 0; 
 const addOne = () => {
-    console.log('addOne')
+    count++;
+    renderCounterApp();
 };
 const minusOne = () => {
     console.log('minusOne');
@@ -29,16 +30,19 @@ const reset = () => {
     console.log('reset');
 };
 
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>reset</button>
-    </div>
-);
-
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    );
 
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp()
