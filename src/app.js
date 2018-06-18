15512560c26ca80c6c5ed84dfe0b44fd1781e046
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-06-18 12:13:20 +1000

    add ternary operators

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ console.log('App.js is running!')
 
 const app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 }; 
 
 const template = (
 <div>
     <h1>{app.title}</h1> 
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
     <ol>
         <li>Item one</li>
         <li>Item two</li>
     </ol>
 </div>
 );
+
 
 const user = {
     name: 'Serina',
@@ -37,5 +40,5 @@ const templateTwo = (
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
 
