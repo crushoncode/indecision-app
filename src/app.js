0fb97e390175156613ec9d220a3b26129ff206c4
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-06-24 14:41:28 +1000

    convert some class based components to stateless functional components

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component {
         this.handlePick = this.handlePick.bind(this)
         this.handleAddOption = this.handleAddOption.bind(this)
         this.state = {
-            options: []
+            options: props.options
  
         };
     }
@@ -38,13 +38,12 @@ class IndecisionApp extends React.Component {
         });
 
     }
-    render() {
-        const title = 'Indecision';
+    render() { 
         const subtitle = 'Put your life in the hands of a computer';
         
         return (
             <div>
-                <Header title={title} subtitle={subtitle} />
+                <Header subtitle={subtitle} />
                 <Action 
                     hasOptions={this.state.options.length > 0} 
                     handlePick={this.handlePick}
@@ -65,52 +64,53 @@ class IndecisionApp extends React.Component {
     }
 }
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
-            </div>
+IndecisionApp.defaultProps = {
+    options: []
+};
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            {props.subtitle && <h2>{props.subtitle}</h2>} 
+        </div>
         );
-    }
+};
+
+Header.defaultProps = {
+    title: 'Indecision'
 }
 
-class Action extends React.Component {
-    render() {
-        return (
-            <div>
-                <button 
-                onClick={this.props.handlePick}
-                disabled={!this.props.hasOptions}
-                >
-                What should I do?</button>
+const Action = (props) => {
+    return (
+        <div>
+            <button
+            onClick={props.handlePick}
+            disabled={!props.hasOptions}
+            >
+            What should I do?
+            </button>
+        </div>        
+    );
+};
+
+const Options = (props) => {
+    return (
+        <div>
+            <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {
+                props.options.map((option) => <Option key={option} optionText={option}/>)
+            }
             </div>
         );
-    }
 }
 
-class Options extends React.Component {
-    render() {
-        return (
-            <div> 
-                <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-                {
-                    this.props.options.map((option) => <Option key={option} optionText={option}/>)
-                }
-            </div>
-        );
-    }
-} 
-
-class Option extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.optionText}
-            </div>
-        );
-    }
+const Option = (props) => {
+    return (
+        <div>
+            {this.props.optionText}
+        </div>
+    );
 }
 
 class AddOption extends React.Component {
